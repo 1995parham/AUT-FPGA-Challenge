c093922e25bdb6013f2637c5aabd6e977492c5c1
Parham Alvani <parham.alvani@gmail.com>
2015-03-03 00:41:16 +0330

    some tcp functions is added.

--- src/tcp.c
+++ src/tcp.c
@@ -5,8 +5,30 @@
  *
  * [] Creation Date : 24-02-2015
  *
- * [] Last Modified : Tue 24 Feb 2015 10:27:54 PM IRST
+ * [] Last Modified : Tue 03 Mar 2015 12:31:58 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "tcp.h"
+
+static int fd;
+
+void recv_move(char *move, int size)
+{
+	FILE *sck  = fdopen(fd, "r+");
+
+	fgets(move, size, sck);
+}
+
+void send_move(const char *move)
+{
+	FILE *sck = fdopen(fd, "r+");
+
+	fprintf(sck, "%s\n", move);
+	fflush(sck);
+}
--- src/tcp.h
+++ src/tcp.h
@@ -5,8 +5,20 @@
  *
  * [] Creation Date : 24-02-2015
  *
- * [] Last Modified : Tue 24 Feb 2015 10:27:56 PM IRST
+ * [] Last Modified : Tue 03 Mar 2015 12:31:53 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
+#ifndef TCP_H
+#define TCP_H
+
+void connect(const char *ip);
+
+void init_tcp(void);
+
+void recv_move(char *move, int size);
+
+void send_move(const char *move);
+
+#endif
