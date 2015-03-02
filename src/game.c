c6b917eb49f0478a69310f4ca93f866a2ff2aba3
Parham Alvani <parham.alvani@gmail.com>
2015-03-03 02:06:21 +0330

    game object is added.

---
+++ src/game.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : game.c
+ *
+ * [] Creation Date : 03-03-2015
+ *
+ * [] Last Modified : Tue 03 Mar 2015 01:35:09 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+
+void play(void)
+{
+
+}
---
+++ src/game.h
@@ -0,0 +1,12 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : game.h
+ *
+ * [] Creation Date : 03-03-2015
+ *
+ * [] Last Modified : Tue 03 Mar 2015 01:32:55 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
