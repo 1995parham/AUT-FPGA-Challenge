3b605a21b5577ff307a0d3f1002baf4a36fef617
Parham Alvani <parham.alvani@gmail.com>
2015-04-29 18:28:05 +0430

    Test cases is added.

--- test/Serial.py
+++ test/Serial.py
@@ -7,3 +7,15 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+import serial
+
+srl = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS)
+filename = input("Input File Name: ")
+print(filename)
+file = open(filename, 'r')
+file.read(1)
+char = file.read(1)
+while not char.find('}'):
+    srl.write(bytes(char, 'ASCII'))
+    char = file.read(1)
